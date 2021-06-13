0408e82de0d54c41239b1dfb5a037d8660c531e6
Nishan621 <nishanchaulagain3@gmail.com>
2021-06-13 17:08:02 +0545

    Completed bonus challenges

--- EndlessRunner/Assets/Scripts/PlayerController.cs
+++ EndlessRunner/Assets/Scripts/PlayerController.cs
@@ -5,30 +5,84 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
   private Rigidbody playerRb;
-  public float jumpForce = 10.0f;
-  public float gravityModifier;
+  private Animator playerJumpAnimation;
+  public ParticleSystem dirtParticle;
+  public ParticleSystem playerExplosion;
+  public AudioClip jumpSound;
+  public AudioClip crashSound;
+  private AudioSource playerAudio;
+  private float jumpForce = 500.0f;
+  private float gravityModifier = 2.0f;
   public bool isOnGround = true;
+  public bool isOnAir = true;
+  public bool gameOver = false;
+  public bool doubleJumpUsed = false;
+  private float doubleJumpForce = 400.0f;
+  public bool doubleSpeed = false;
 
 
   // Start is called before the first frame update
   void Start()
   {
     playerRb = GetComponent<Rigidbody>();
+    playerJumpAnimation = GetComponent<Animator>();
+    playerAudio = GetComponent<AudioSource>();
     Physics.gravity *= gravityModifier;
   }
 
   // Update is called once per frame
   void Update()
   {
-    if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
-    {
-      playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
-      isOnGround = false;
 
+    if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+    {
+      playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+      isOnGround = false;
+      playerJumpAnimation.SetTrigger("Jump_trig");
+      dirtParticle.Stop();
+      playerAudio.PlayOneShot(jumpSound, 1.0f);
+      doubleJumpUsed = false;
     }
+    else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+    {
+      doubleJumpUsed = true;
+      playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+      playerJumpAnimation.Play("Running_Jump", 3, 0f);
+      playerAudio.PlayOneShot(jumpSound, 1.0f);
+    }
+
+    if (Input.GetKey(KeyCode.LeftShift))
+    {
+      doubleSpeed = true;
+      playerJumpAnimation.SetFloat("Speed_Multiplier", 2.0f);
+    }
+    else if (doubleSpeed)
+    {
+      doubleSpeed = false;
+      playerJumpAnimation.SetFloat("Speed_Multiplier", 1.0f);
+    }
+
+
   }
+
+
   private void OnCollisionEnter(Collision collision)
   {
-    isOnGround = true;
+    if (collision.gameObject.CompareTag("Ground"))
+    {
+      isOnGround = true;
+      dirtParticle.Play();
+    }
+    else if (collision.gameObject.CompareTag("Obstacle"))
+    {
+      Debug.Log("Game Over!");
+      gameOver = true;
+      playerJumpAnimation.SetBool("Death_b", true);
+      playerJumpAnimation.SetInteger("DeathType_int", 1);
+      dirtParticle.Stop();
+      playerExplosion.Play();
+      playerAudio.PlayOneShot(crashSound, 1.0f);
+
+    }
   }
 }
--- EndlessRunner/Assets/Scripts/SpawnManager.cs
+++ EndlessRunner/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-  public GameObject obstaclePrefab;
-  private Vector3 spawnPos = new Vector3(25, 0, 0);
+  public GameObject[] obstaclePrefab;
+  private Vector3 spawnPos = new Vector3(35, 0, 0);
   public float startDelay = 2.0f;
   public float repeatRate = 2.0f;
+  private PlayerController playerControllerScript;
 
   // Start is called before the first frame update
   void Start()
   {
     InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
   }
 
   // Update is called once per frame
@@ -22,6 +24,11 @@ public class SpawnManager : MonoBehaviour
   }
   void SpawnObstacle()
   {
-    Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    if (playerControllerScript.gameOver == false)
+    {
+      int obstacleIndex = Random.Range(0, obstaclePrefab.Length);
+      Instantiate(obstaclePrefab[obstacleIndex], spawnPos, obstaclePrefab[obstacleIndex].transform.rotation);
+    }
+
   }
 }
